90ff64b6827414a7b36984cf4f53d9eee8371870
jjh005 <74509829+jjh005@users.noreply.github.com>
2020-11-30 09:08:53 -0300

    se agrego codigo para ver como se desempeña el algoritmo para distinto numero de ciudades

--- Taller2/src/Main.java
+++ Taller2/src/Main.java
@@ -1,4 +1,7 @@
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -9,6 +12,7 @@ public class Main {
 
 
 	public static void main(String[] args) {
+		//medir(350);
 		
 		//lineas de codigo que pida por consola el numero n de ciudades
 		Scanner reader = new Scanner(System.in);
@@ -30,7 +34,7 @@ public class Main {
 		Date date = new Date();
 	    //obtener la hora y salida por pantalla con formato, para mostrar el tiempo de ejecucion de Hill-Climbing
 	    DateFormat hourFormat = new SimpleDateFormat("HH:mm:ss");
-	    System.out.println("Timpo de inicio Hill-Climbing: "+hourFormat.format(date));
+	    System.out.println("Tiempo de inicio Hill-Climbing: "+hourFormat.format(date));
 	    
 		
 		
@@ -42,7 +46,7 @@ public class Main {
 	    
 	    long endTime = System.nanoTime();
 	    tiempo=(endTime-startTime);
-	    //cc.getTiempoXIteracion().addFirst(Math.toIntExact(tiempo));
+	    date = new Date();
 	    
 		System.out.println("\nLA RUTA ES:");
 		System.out.println(cc.generarStringRuta());
@@ -66,6 +70,58 @@ public class Main {
 		 
 		
 	}
+	
+	public static void medir(int numeroPruebas) {
+		int npruebas=numeroPruebas+3;
+		BufferedWriter bw = null;
+		FileWriter fw = null;
+		try {
+		    String ruta = "pruebas.txt";
+		    	
+		    fw=new FileWriter(ruta);
+		    bw=new BufferedWriter(fw);
+		    System.out.println("Ejecutando...");
+		    int porcentajeAnt=1;
+		    int i=3;
+		    
+		    
+		    for(i=3;i<npruebas;i++) {
+		    	int x=(i*100)/npruebas;
+		    	if(porcentajeAnt!=x) {
+		    		System.out.println(x+"%");
+		    	}
+		    	porcentajeAnt=x;
+		    	
+				Ruta cc=new Ruta("ciudades.txt",i);
+				LinkedList<Ciudad> cities=cc.getCiudades();
+				Ciudad randCity=cities.get(getNumeroAleatorio(0,i-1));
+				long tiempo;
+				
+			    long startTime = System.nanoTime();
+			    
+			    cc.hillClimbing(randCity);//***Aqui se ejecuta Hill-Climbing
+			    
+			    long endTime = System.nanoTime();
+			    tiempo=(endTime-startTime);
+			    bw.write(tiempo+"\n");
+			}
+		    System.out.println(porcentajeAnt+1+"%");
+		    
+		     } catch (Exception e) {
+		    	 System.out.println("Matrix is empty!!");
+		    	 }finally{
+		    		 try {
+		    			 if (bw != null)
+		                 bw.close();
+		    			 if (fw != null)
+		                 fw.close();
+		    			 }catch (IOException ex){
+		    				 ex.printStackTrace();
+		    				 }
+		    		 }
+		}
+		
+		
 	
 	public static int getNumeroAleatorio(int min, int max) {
 	    return (int) ((Math.random() * (max - min)) + min);
